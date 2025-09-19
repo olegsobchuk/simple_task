34b13f8b9892e9436b3619332084e500b02b2e45
Oleh Sobchuk <oleg.sobchuk@gmail.com>
2025-09-19 14:49:50 +0300

    Golang add task03

---
+++ golang/task_03/main.rb
@@ -0,0 +1,19 @@
+# Given an array of audio file durations, write a function to group the files into playlists such that each playlist's total duration does not exceed a given limit maxDuration. Return an array of playlists, where each playlist is an array of file durations. Try to minimize the number of playlists.
+
+# Example:
+
+# ```
+# const files = [120, 90, 60, 150, 80];
+# const maxDuration = 200;
+
+# groupAudioFiles(files, maxDuration)
+# > [[150], [120,80], [90,60]]
+
+# groupAudioFiles(files, 160)
+# > [[150], [120], [90,60], [80]]
+
+# ```
+
+func groupAudioFiles(files []uint, duration uint) [][]int {
+
+}
